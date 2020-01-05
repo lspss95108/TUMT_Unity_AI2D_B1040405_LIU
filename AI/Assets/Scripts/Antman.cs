657374341220afbabba045ec7e7551060f475ee2
lspss95108 <55518448+lspss95108@users.noreply.github.com>
2020-01-06 03:01:33 +0800

    fffffff

---
+++ AI/Assets/Scripts/Antman.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;                  // 引用 Unity API - API 倉庫 功能、工具
+using UnityEngine.Events;           // 引用 事件 API
+using UnityEngine.UI;               // 引用 介面 API
+
+public class Antman : MonoBehaviour    // 類別 類別名稱
+{
+    #region 欄位
+    // 成員：欄位、屬性、方法、事件
+    // 修飾詞 類型 名稱 指定 值；
+    [Header("移動速度"), Range(0, 1000)]
+    public int speed = 50;                  // 整數
+    [Header("跳躍高度"), Range(0, 2000)]
+    public float jump = 2.5f;               // 浮點數
+    [Header("血量"), Range(0, 200)]
+    public float hp = 100;
+    [Header("血量吧條")]
+    public Image hpBar;
+    [Header("結束畫面")]
+    public GameObject final;
+    [Header("音效區域")]
+    public AudioClip soundProp;
+    [Header("吃東西事件")]
+    public UnityEvent onEat;
+
+    //public string AntmanName = "狐狸";         // 字串
+    //public bool pass = false;               // 布林值 - true/false
+
+    //private Transform tra;
+    private Rigidbody2D r2d;
+    private AudioSource aud;
+    private Animator ani;
+    private float hpMax;
+    private bool isGround;
+    #endregion
+
+    #region 事件
+    // 事件：在特定時間點會以指定頻率執行的方法
+    // 開始事件：遊戲開始時執行一次
+    private void Start()
+    {
+        // 泛型 <T>
+        //tra = GetComponent<Transform>();
+        r2d = GetComponent<Rigidbody2D>();
+        aud = GetComponent<AudioSource>();
+        ani = GetComponent<Animator>();
+
+        hpMax = hp;
+    }
+
+    // 更新事件：每秒執行約 60 次
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) Turn();
+        if (Input.GetKeyDown(KeyCode.A)) Turn(180);
+        Jump();
+    }
+
+    // 固定更新事件：每禎 0.002 秒
+    private void FixedUpdate()
+    {
+        Walk(); // 呼叫方法
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "地板" || collision.gameObject.tag == "老鼠")
+        {
+            isGround = true;
+            ani.SetBool("跳躍開關", false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "金幣")
+        {
+            aud.PlayOneShot(soundProp, 1.2f);
+            Destroy(collision.gameObject);  // 刪除
+            onEat.Invoke();                 // 呼叫事件
+        }
+
+        if (collision.name == "死亡區域") Dead();
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
+        if (r2d.velocity.magnitude < 10)
+            r2d.AddForce(new Vector2(speed * Input.GetAxisRaw("Horizontal"), 0));
+        ani.SetBool("跑步開關", Input.GetAxisRaw("Horizontal") != 0);
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0, jump));
+            ani.SetBool("跳躍開關", true);
+        }
+    }
+
+    // 參數語法：類型 名稱
+    /// <summary>
+    /// 轉彎
+    /// </summary>
+    /// <param name="direction">方向，左轉為 180，右轉為 0</param>
+    private void Turn(int direction = 0)
+    {
+        transform.eulerAngles = new Vector3(0, direction, 0);
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        hpBar.fillAmount = hp / hpMax;
+
+        if (hp <= 0) Dead();
+    }
+
+    private void Dead()
+    {
+        final.SetActive(true);
+    }
+    #endregion
+}
---
+++ AI/Assets/Scripts/NPC.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.UI;       // 引用 介面 API
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    #region 欄位
+    // 定義列舉
+    // 修飾詞 列舉 列舉名稱 { 列舉內容, .... }
+    public enum state
+    {
+        // 一般、尚未完成、完成
+        start, notComplete, complete
+    }
+    // 使用列舉
+    // 修飾詞 類型 名稱
+    public state _state;
+
+    [Header("對話文字")]
+    public string sayStart = "喂!!!我要蒐集十顆金幣!!!";
+    public string sayNotComplete = "還沒找到十顆金幣嗎!!!";
+    public string sayComplete = "感謝找到金幣!!!";
+    [Range(0.001f, 1.5f)]
+    public float speed = 1.5f;
+    [Header("任務相關")]
+    public bool complete;
+    public int countPlayer;
+    public int countFinish = 10;
+    [Header("介面")]
+    public GameObject objCanvas;
+    public Text textSay;
+
+    #endregion
+
+    #region 事件
+    
+
+    // 2D 觸發事件
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 如果碰到物件為"玩家"
+        if (collision.name == "蟻人")
+            objCanvas.SetActive(true);
+        textSay.text = sayStart;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "蟻人")
+            SayClose();
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 對話：打字效果
+    /// </summary>
+    private void Say()
+    {
+        // 畫布.顯示
+        
+        StopAllCoroutines();
+
+        if (countPlayer >= countFinish) _state = state.complete;
+
+
+        // 判斷式(狀態)
+        switch (_state)
+        {
+            case state.start:
+                StartCoroutine(ShowDialog(sayStart));           // 開始對話
+                _state = state.notComplete;
+                break;
+            case state.notComplete:
+                StartCoroutine(ShowDialog(sayNotComplete));     // 開始對話未完成
+                break;
+            case state.complete:
+                StartCoroutine(ShowDialog(sayComplete));        // 開始對話完成
+                break;
+        }
+    }
+
+    private IEnumerator ShowDialog(string say)
+    {
+        textSay.text = "";                              // 清空文字
+
+        for (int i = 0; i < say.Length; i++)            // 迴圈跑對話.長度
+        {
+            textSay.text += say[i].ToString();          // 累加每個文字
+            yield return new WaitForSeconds(speed);     // 等待
+        }
+    }
+
+    /// <summary>
+    /// 關閉對話
+    /// </summary>
+    private void SayClose()
+    {
+        StopAllCoroutines();
+        objCanvas.SetActive(false);
+    }
+
+    /// <summary>
+    /// 玩家取得道具
+    /// </summary>
+    public void PlayerGet()
+    {
+        countPlayer++;
+    }
+    #endregion
+}
